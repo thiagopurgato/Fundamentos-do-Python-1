86eb93c20ae01409053979657a2379f43ee05729
thiagopurgato <thiagopurgato@gmail.com>
2025-12-04 17:55:23 -0300

    Add files via upload

---
+++ Estrutura de Dados.py
@@ -0,0 +1,6 @@
+#1 - Crie um programa para efetuar a leitura de um número real
+#e apresentar o resultado do cubo deste número.
+a= float(input("Digite um número:"))
+b= a ** 3.0
+print(a, "elevado a 3 é", b)
+#22 - Crie 3 variáveis para armazenar nomes de frutas. Em seguida, concatene-as.
