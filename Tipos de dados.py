05230c29e07883564dc1f21289a61630afd7f40d
thiagopurgato <thiagopurgato@gmail.com>
2025-12-04 16:50:56 -0300

    Add files via upload

---
+++ Tipos de dados.py
@@ -0,0 +1,78 @@
+
+# objetivo mostrar o que esta dentro do parenteses dela no
+# no ambiente de teste 
+# 4  tipos strings float inteiro boleanos
+
+# quebra de linha em python (pular linha) 
+print('isso é\num texto ')
+print()
+print('''
+hdsjka
+klda
+dhasjkld
+dasjkd
+''')
+
+print(10)
+print(10.5)
+print(True, False)
+print('\n')
+print('ola seja\nbem vindo')
+# concatenação separar/organizar os dados
+print('a', 'b', 'c')
+# aritmético -  sempre um número como saída *** 
+print(10 + 2)
+print(10 - 2)
+print(10 / 2)
+print(10 ** 2)
+print(10 * 2)
+print(10.3 // 2)
+print(10 % 2)
+print(16 ** 0.5)
+# lógicos  -  True ou False
+print(10 > 2) # maior
+print(10 < 2) # menor
+print(10 >= 2) # maior ou igual
+print(10 <= 2) # menor ou igual
+print(10 == 2) # comparação
+print(10 != 2) # diferente
+
+# estrutura de dado  palavra  =
+# fracamente tipadas
+# dinamicamente
+# únicas
+# CRIE
+# atribuição
+# 1  dado
+
+numero_1 = 10
+numero_2 = 20
+soma  =  numero_1 + numero_2
+print(soma)
+
+altura  = 1.70
+peso =  70
+imc  =  peso/(altura ** 2)
+print('IMC:', imc)
+
+
+nome     = 'José'
+idade    =  35
+endereco =  'Rua 10, jd x' 
+email =  'jose@gmail.com'
+print('Nome:', nome)
+print('Idade:',idade)
+print('Endereço:', endereco)
+print('E-mail:',email)
+
+print(f'''
+Nome: {nome}
+Idade: {idade}
+Endereço:{endereco}
+E-mail:{email}
+''')
+john = 3
+
+
+
+
